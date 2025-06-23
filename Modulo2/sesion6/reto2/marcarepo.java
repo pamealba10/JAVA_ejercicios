d66da70f5ba85819d41f2670ed8e469ca2253b1e
Pame Alba <106121745+pamealba10@users.noreply.github.com>
2025-06-22 18:12:47 -0600

    Create marcarepo.java

---
+++ Modulo2/sesion6/reto2/marcarepo.java
@@ -0,0 +1,9 @@
+package com.bedu.inventario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+// Esta interfaz extiende JpaRepository para gestionar operaciones CRUD sobre la entidad Producto
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
+}
